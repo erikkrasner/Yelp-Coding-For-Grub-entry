aa8e77cbb05702faa15343ae1b70516f41eb7606
Erik <erik@Eriks-MacBook-Pro-3.local>
2012-03-16 20:10:23 -0700

    added json parsing

--- yelp_classifier.py
+++ yelp_classifier.py
@@ -1,10 +1,10 @@
 import json
 
 def analyze_review_sentiment(review, sentiments):
-    average = None
+    average = 0
     sentiment=0
     word_count=0
-    for word in tweet_words:
+    for word in review.split():
         if word in sentiments:
             sentiment += sentiments[word]
             word_count+=1
@@ -15,18 +15,37 @@ def analyze_review_sentiment(review, sentiments):
 def make_sentiments_table():
     sentiments_table = {}
     for line in open('sentiments.csv'):
-	word, sentiment = line.split(',')
-	sentiment = float(sentiment)
-	sentiments_table[word] = sentiment
+        word, sentiment = line.split(',')
+        sentiment = float(sentiment)
+        sentiments_table[word] = sentiment
     return sentiments_table
-        
+
+def star_from_sentiments(review, sentiments_table):
+    return 3 + 2 * analyze_review_sentiment(review, sentiments_table)
+
+def predict_reviews(review_data,sentiments_table):
+    output = {}
+    first_five_reviews = 50
+    for raw_review in review_data:
+        # Each line of the testing data is a JSON dictionary object containing a review record.6
+        # Use simplejson's loads() function to parse this into a native Python dictionary.
+        review = json.loads(raw_review)
+        review_id = review.get('review_id', None)
+        if review_id is None:
+            continue
+        review_text = review.get('text', None)
+        guess = star_from_sentiments(review_text, sentiments_table)
+        output[review_id] = guess
+    print json.dumps(output)
+    
+
+    
 
 def train_and_predict(training_filename, test_filename):
-    with open(training_filename) as training_file:
-        user_averages, business_averages = generate_model(training_file)
+    sentiments_table = make_sentiments_table()
 
     with open(test_filename) as test_file:
-        predict_reviews(test_file, user_averages, business_averages)
+        predict_reviews(test_file, sentiments_table)
 
 if __name__ == '__main__':
     if len(argv) != 3:
@@ -34,5 +53,4 @@ if __name__ == '__main__':
     training_filename = argv[1]
     test_filename = argv[2]
 
-    
     train_and_predict(training_filename, test_filename)
